78fbf9bcce7e3fecae5fbadd08a3ceadd9d4a2fa
Drew Ymker <drewymker@gmail.com>
2026-05-07 14:01:03 -0400

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,22 @@
 module.exports = {
   siteUrl: 'https://cleanwithpremier.com',
   generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  additionalPaths: async (config) => {
+    const homepageSections = [
+      '/#services',
+      '/#why-us',
+      '/#testimonials',
+      '/#service-area',
+      '/#contact',
+    ]
+
+    return homepageSections.map((path) => ({
+      loc: path,
+      changefreq: 'weekly',
+      priority: 0.6,
+      lastmod: new Date().toISOString(),
+    }))
+  },
 }
